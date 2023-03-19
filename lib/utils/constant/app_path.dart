620acde8b62bd193ef0a491f9e185ec912d5f7b3
Mehdi Fessiane <mehdi.fessiane@epitech.eu>
2023-03-19 23:18:34 +0100

    Base of App

---
+++ lib/utils/constant/app_path.dart
@@ -0,0 +1,6 @@
+class AppPath {
+  static const String basePath = '/';
+  static const String homeScreenPath = '/home';
+  static const String loginScreenPath = '/login';
+  static const String profileScreenPath = '/profile';
+}
